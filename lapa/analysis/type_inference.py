3822183228ad97977ce94e8218e9a96fa9d6f6a8
sungwookim <rkwk0112@gmail.com>
2024-11-24 16:58:04 +0900

    Enhance type inference analyzer

--- lapa/analysis/type_inference.py
+++ lapa/analysis/type_inference.py
@@ -4,7 +4,7 @@ Type Inference Analysis module.
 This module provides functionality to perform type inference on the IR.
 """
 
-from typing import Dict, Any, Optional
+from typing import Dict, Any, Optional, List
 from lapa.ir import IR, IRNode, IRNodeType
 
 
@@ -69,44 +69,28 @@ class TypeInferenceAnalyzer:
             inferred_type = self.infer_variable_type(node)
         elif node.node_type == IRNodeType.BINARY_OPERATION:
             # For binary operations, infer type based on operands
-            left_operand = node.attributes.get('left_operand')
-            right_operand = node.attributes.get('right_operand')
-            operator = node.attributes.get('operator')
-
-            left_type = self.infer_type(left_operand) if left_operand else 'unknown'
-            right_type = self.infer_type(right_operand) if right_operand else 'unknown'
-
-            inferred_type = self.resolve_binary_operation_type(operator, left_type, right_type)
+            inferred_type = self.infer_binary_operation_type(node)
         elif node.node_type == IRNodeType.FUNCTION_CALL:
             # For function calls, infer type based on function definition
             inferred_type = self.infer_function_call_type(node)
         elif node.node_type == IRNodeType.ASSIGNMENT:
             # For assignments, infer type from the right-hand side
-            right = node.attributes.get('right')
-            left = node.attributes.get('left')
-            inferred_type = self.infer_type(right)
-            # Update the variable's type information
-            if isinstance(left, IRNode):
-                self.type_information[left] = inferred_type
+            inferred_type = self.infer_assignment_type(node)
         elif node.node_type == IRNodeType.CONDITIONAL:
             # For conditionals, infer type based on branches
-            condition = node.attributes.get('condition')
-            true_branch = node.attributes.get('true_branch')
-            false_branch = node.attributes.get('false_branch')
-            self.infer_type(condition)
-            self.infer_type(true_branch)
-            self.infer_type(false_branch)
-            inferred_type = 'void'
+            inferred_type = self.infer_conditional_type(node)
         elif node.node_type == IRNodeType.LOOP:
             # For loops, infer type of condition and body
-            condition = node.attributes.get('condition')
-            body = node.attributes.get('body')
-            self.infer_type(condition)
-            self.infer_type(body)
-            inferred_type = 'void'
+            inferred_type = self.infer_loop_type(node)
         elif node.node_type == IRNodeType.FUNCTION_DEF:
             # Infer return type and parameter types
             inferred_type = self.infer_function_def_type(node)
+        elif node.node_type == IRNodeType.CLASS_DEF:
+            # Infer types within class definitions
+            inferred_type = self.infer_class_def_type(node)
+        elif node.node_type == IRNodeType.COLLECTION:
+            # Infer type for collections like lists, dicts, sets, tuples
+            inferred_type = self.infer_collection_type(node)
         else:
             # For other node types, default to 'unknown'
             inferred_type = 'unknown'
@@ -121,7 +105,7 @@ class TypeInferenceAnalyzer:
         """Infer type for a literal node."""
         # Assume node.attributes['value'] contains the literal value
         value = node.attributes.get('value')
-        if isinstance(value, bool):  # Check for bool first since bool is a subclass of int
+        if isinstance(value, bool):
             return 'bool'
         elif isinstance(value, int):
             return 'int'
@@ -129,6 +113,14 @@ class TypeInferenceAnalyzer:
             return 'float'
         elif isinstance(value, str):
             return 'str'
+        elif isinstance(value, list):
+            return f'List[{self.infer_collection_element_type(value)}]'
+        elif isinstance(value, dict):
+            return f'Dict[{self.infer_dict_key_value_types(value)}]'
+        elif isinstance(value, tuple):
+            return f'Tuple[{", ".join(self.infer_tuple_element_types(value))}]'
+        elif isinstance(value, set):
+            return f'Set[{self.infer_collection_element_type(value)}]'
         else:
             return 'unknown'
 
@@ -143,6 +135,145 @@ class TypeInferenceAnalyzer:
             return inferred_type
         else:
             return 'unknown'
+
+    def infer_binary_operation_type(self, node: IRNode) -> Any:
+        """Infer type for a binary operation node."""
+        left_operand = node.attributes.get('left_operand')
+        right_operand = node.attributes.get('right_operand')
+        operator = node.attributes.get('operator')
+
+        left_type = self.infer_type(left_operand) if left_operand else 'unknown'
+        right_type = self.infer_type(right_operand) if right_operand else 'unknown'
+
+        return self.resolve_binary_operation_type(operator, left_type, right_type)
+
+    def infer_function_call_type(self, node: IRNode) -> Any:
+        """Infer type for a function call node."""
+        function_def = node.attributes.get('function')
+        if function_def:
+            # Return the function's return type directly if available
+            return_type = function_def.attributes.get('return_type', 'unknown')
+            return return_type
+        return 'unknown'
+
+    def infer_assignment_type(self, node: IRNode) -> Any:
+        """Infer type for an assignment node."""
+        right = node.attributes.get('right')
+        left = node.attributes.get('left')
+        inferred_type = self.infer_type(right)
+        # Update the variable's type information
+        if isinstance(left, IRNode):
+            self.type_information[left] = inferred_type
+        return inferred_type
+
+    def infer_conditional_type(self, node: IRNode) -> Any:
+        """Infer type for a conditional node."""
+        condition = node.attributes.get('condition')
+        true_branch = node.attributes.get('true_branch')
+        false_branch = node.attributes.get('false_branch')
+
+        self.infer_type(condition)
+        true_type = self.infer_type(true_branch)
+        false_type = self.infer_type(false_branch)
+
+        if true_type == false_type:
+            return true_type
+        else:
+            return 'unknown'  # Could be improved with union types
+
+    def infer_loop_type(self, node: IRNode) -> Any:
+        """Infer type for a loop node."""
+        condition = node.attributes.get('condition')
+        body = node.attributes.get('body')
+        self.infer_type(condition)
+        self.infer_type(body)
+        return 'void'
+
+    def infer_function_def_type(self, node: IRNode) -> Any:
+        """Infer return type for a function definition."""
+        # Infer types of parameters
+        parameters = node.attributes.get('parameters', [])
+        for param in parameters:
+            self.infer_type(param)
+        # Infer type of the function body if it exists
+        body = node.attributes.get('body')
+        if body is not None:
+            self.infer_type(body)
+        # Get the return type if specified
+        return_type = node.attributes.get('return_type', 'void')
+        self.type_information[node] = return_type
+        return return_type
+
+    def infer_class_def_type(self, node: IRNode) -> Any:
+        """Infer types within a class definition."""
+        class_name = node.attributes.get('name', 'UnknownClass')
+        # Infer types of attributes and methods
+        members = node.attributes.get('members', [])
+        for member in members:
+            self.infer_type(member)
+        self.type_information[node] = class_name
+        return class_name
+
+    def infer_collection_type(self, node: IRNode) -> Any:
+        """Infer type for collection nodes."""
+        collection_type = node.attributes.get('collection_type')
+        elements = node.attributes.get('elements', [])
+        element_types = [self.infer_type(element) for element in elements]
+        element_type = self.unify_types(element_types)
+
+        if collection_type == 'list':
+            return f'List[{element_type}]'
+        elif collection_type == 'dict':
+            key_types = []
+            value_types = []
+            for key, value in elements:
+                key_types.append(self.infer_type(key))
+                value_types.append(self.infer_type(value))
+            key_type = self.unify_types(key_types)
+            value_type = self.unify_types(value_types)
+            return f'Dict[{key_type}, {value_type}]'
+        elif collection_type == 'set':
+            return f'Set[{element_type}]'
+        elif collection_type == 'tuple':
+            return f'Tuple[{", ".join(element_types)}]'
+        else:
+            return 'unknown'
+
+    def unify_types(self, types: List[Any]) -> Any:
+        """Unify a list of types into a single type."""
+        types_set = set(types)
+        if len(types_set) == 1:
+            return types[0]
+        elif 'unknown' in types_set:
+            types_set.discard('unknown')
+            if len(types_set) == 1:
+                return types_set.pop()
+            else:
+                return 'unknown'
+        else:
+            return 'Any'  # Could be improved with union types or type variables
+
+    def infer_collection_element_type(self, collection: Any) -> Any:
+        """Infer the type of elements within a collection."""
+        element_types = [self.infer_literal_type(IRNode(attributes={'value': element}, node_type=IRNodeType.LITERAL)) for element in collection]
+        return self.unify_types(element_types)
+
+    def infer_dict_key_value_types(self, dictionary: dict) -> str:
+        """Infer the types of keys and values in a dictionary."""
+        key_types = []
+        value_types = []
+        for key, value in dictionary.items():
+            key_type = self.infer_literal_type(IRNode(attributes={'value': key}, node_type=IRNodeType.LITERAL))
+            value_type = self.infer_literal_type(IRNode(attributes={'value': value}, node_type=IRNodeType.LITERAL))
+            key_types.append(key_type)
+            value_types.append(value_type)
+        unified_key_type = self.unify_types(key_types)
+        unified_value_type = self.unify_types(value_types)
+        return f'{unified_key_type}, {unified_value_type}'
+
+    def infer_tuple_element_types(self, tuple_obj: tuple) -> List[str]:
+        """Infer the types of elements in a tuple."""
+        return [self.infer_literal_type(IRNode(attributes={'value': element}, node_type=IRNodeType.LITERAL)) for element in tuple_obj]
 
     def resolve_binary_operation_type(self, operator: str, left_type: Any, right_type: Any) -> Any:
         """Resolve the type of a binary operation based on operand types."""
@@ -165,33 +296,9 @@ class TypeInferenceAnalyzer:
                     return 'bool'
             if 'int' in (left_type, right_type):
                 if operator in ['+', '-', '*', '/']:
-                    return 'int'
+                    return 'float' if 'float' in (left_type, right_type) else 'int'
                 elif operator in ['==', '!=', '<', '<=', '>', '>=']:
                     return 'bool'
             return 'unknown'
-
-    def infer_function_call_type(self, node: IRNode) -> Any:
-        """Infer type for a function call node."""
-        # Get the function definition from node.attributes['function']
-        function_def = node.attributes.get('function')
-        if function_def:
-            # Return the function's return type directly if available
-            return function_def.attributes.get('return_type', 'unknown')
-        return 'unknown'
-
-    def infer_function_def_type(self, node: IRNode) -> Any:
-        """Infer return type for a function definition."""
-        # Infer types of parameters
-        parameters = node.attributes.get('parameters', [])
-        for param in parameters:
-            self.infer_type(param)
-        # Infer type of the function body if it exists
-        body = node.attributes.get('body')
-        if body is not None:
-            self.infer_type(body)
-        # Get the return type if specified
-        return_type = node.attributes.get('return_type', 'void')
-        self.type_information[node] = return_type
-        return return_type
 
     # Additional methods can be added here to handle more complex cases
--- tests/analysis/test_type_inference.py
+++ tests/analysis/test_type_inference.py
@@ -6,6 +6,7 @@ import unittest
 from lapa.ir import IR, IRNode, IRNodeType
 from lapa.analysis.type_inference import TypeInferenceAnalyzer
 
+
 class TestTypeInferenceAnalyzer(unittest.TestCase):
     def setUp(self):
         self.analyzer = TypeInferenceAnalyzer()
@@ -13,201 +14,195 @@ class TestTypeInferenceAnalyzer(unittest.TestCase):
 
     def test_empty_ir(self):
         """Test type inference on an empty IR."""
-        try:
-            self.analyzer.analyze(self.ir)
-        except Exception as e:
-            self.fail(f"TypeInferenceAnalyzer raised an exception on empty IR: {e}")
-
-        # Ensure that type_information is empty
+        self.analyzer.analyze(self.ir)
         self.assertEqual(len(self.analyzer.type_information), 0)
 
     def test_literal_type_inference(self):
-        """Test type inference for literal nodes."""
-        # Integer literal
-        int_node = IRNode(IRNodeType.LITERAL)
-        int_node.attributes['value'] = 42
-        self.assertEqual(self.analyzer.infer_literal_type(int_node), 'int')
-
-        # Float literal
-        float_node = IRNode(IRNodeType.LITERAL)
-        float_node.attributes['value'] = 3.14
-        self.assertEqual(self.analyzer.infer_literal_type(float_node), 'float')
-
-        # String literal
-        str_node = IRNode(IRNodeType.LITERAL)
-        str_node.attributes['value'] = "hello"
-        self.assertEqual(self.analyzer.infer_literal_type(str_node), 'str')
-
-        # Boolean literal
-        bool_node = IRNode(IRNodeType.LITERAL)
-        bool_node.attributes['value'] = True
-        self.assertEqual(self.analyzer.infer_literal_type(bool_node), 'bool')
-
-        # Unknown literal
-        unknown_node = IRNode(IRNodeType.LITERAL)
-        unknown_node.attributes['value'] = None
-        self.assertEqual(self.analyzer.infer_literal_type(unknown_node), 'unknown')
+        """Test type inference for literals."""
+        literal_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 42}
+        )
+        self.ir.root.add_child(literal_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(literal_node)
+        self.assertEqual(inferred_type, 'int')
 
     def test_variable_type_inference(self):
-        """Test type inference for variable nodes."""
-        # Variable with definition
-        var_node = IRNode(IRNodeType.VARIABLE)
-        def_node = IRNode(IRNodeType.LITERAL)
-        def_node.attributes['value'] = 42
-        var_node.attributes['definition'] = def_node
-        
-        self.assertEqual(self.analyzer.infer_variable_type(var_node), 'int')
-
-        # Variable without definition
-        undefined_var = IRNode(IRNodeType.VARIABLE)
-        self.assertEqual(self.analyzer.infer_variable_type(undefined_var), 'unknown')
+        """Test type inference for variables."""
+        var_node = IRNode(
+            node_type=IRNodeType.VARIABLE, attributes={'name': 'x'}
+        )
+        assign_node = IRNode(
+            node_type=IRNodeType.ASSIGNMENT,
+            attributes={'left': var_node, 'right': IRNode(
+                node_type=IRNodeType.LITERAL, attributes={'value': 3.14}
+            )},
+        )
+        self.ir.root.add_child(assign_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(var_node)
+        self.assertEqual(inferred_type, 'float')
 
     def test_binary_operation_type_inference(self):
         """Test type inference for binary operations."""
-        # Test same type operands
-        self.assertEqual(
-            self.analyzer.resolve_binary_operation_type('+', 'int', 'int'),
-            'int'
+        left_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 5}
         )
-        self.assertEqual(
-            self.analyzer.resolve_binary_operation_type('>', 'int', 'int'),
-            'bool'
+        right_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 10}
         )
-
-        # Test type promotion
-        self.assertEqual(
-            self.analyzer.resolve_binary_operation_type('+', 'int', 'float'),
-            'float'
+        bin_op_node = IRNode(
+            node_type=IRNodeType.BINARY_OPERATION,
+            attributes={
+                'operator': '+',
+                'left_operand': left_node,
+                'right_operand': right_node,
+            },
         )
-
-        # Test comparison operators
-        self.assertEqual(
-            self.analyzer.resolve_binary_operation_type('==', 'int', 'float'),
-            'bool'
-        )
-
-        # Test unknown type handling
-        self.assertEqual(
-            self.analyzer.resolve_binary_operation_type('+', 'unknown', 'int'),
-            'unknown'
-        )
+        self.ir.root.add_child(bin_op_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(bin_op_node)
+        self.assertEqual(inferred_type, 'int')
 
     def test_function_call_type_inference(self):
         """Test type inference for function calls."""
-        # Create a function definition node
-        func_def = IRNode(IRNodeType.FUNCTION_DEF)
-        func_def.attributes['return_type'] = 'int'
-
-        # Create a function call node
-        func_call = IRNode(IRNodeType.FUNCTION_CALL)
-        func_call.attributes['function'] = func_def
-
-        self.assertEqual(self.analyzer.infer_function_call_type(func_call), 'int')
-
-        # Test function call without definition
-        undefined_call = IRNode(IRNodeType.FUNCTION_CALL)
-        self.assertEqual(self.analyzer.infer_function_call_type(undefined_call), 'unknown')
+        func_def_node = IRNode(
+            node_type=IRNodeType.FUNCTION_DEF,
+            attributes={'name': 'add', 'return_type': 'int'},
+        )
+        func_call_node = IRNode(
+            node_type=IRNodeType.FUNCTION_CALL,
+            attributes={'function': func_def_node},
+        )
+        self.ir.root.add_child(func_call_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(func_call_node)
+        self.assertEqual(inferred_type, 'int')
 
     def test_assignment_type_inference(self):
-        """Test type inference for assignments."""
-        # Create nodes for assignment
-        assign_node = IRNode(IRNodeType.ASSIGNMENT)
-        left_node = IRNode(IRNodeType.VARIABLE)
-        right_node = IRNode(IRNodeType.LITERAL)
-        right_node.attributes['value'] = 42
-        assign_node.attributes['left'] = left_node
-        assign_node.attributes['right'] = right_node
-
-        # Infer type
-        inferred_type = self.analyzer.infer_type(assign_node)
-        self.assertEqual(inferred_type, 'int')
-        # Check that the variable's type was updated
-        self.assertEqual(self.analyzer.type_information[left_node], 'int')
+        """Test type inference in assignments."""
+        var_node = IRNode(
+            node_type=IRNodeType.VARIABLE, attributes={'name': 'message'}
+        )
+        literal_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 'Hello'}
+        )
+        assign_node = IRNode(
+            node_type=IRNodeType.ASSIGNMENT,
+            attributes={'left': var_node, 'right': literal_node},
+        )
+        self.ir.root.add_child(assign_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(var_node)
+        self.assertEqual(inferred_type, 'str')
 
     def test_conditional_type_inference(self):
-        """Test type inference for conditional statements."""
-        # Create nodes for conditional
-        cond_node = IRNode(IRNodeType.CONDITIONAL)
-        condition = IRNode(IRNodeType.LITERAL)
-        condition.attributes['value'] = True
-        true_branch = IRNode(IRNodeType.LITERAL)
-        true_branch.attributes['value'] = 1
-        false_branch = IRNode(IRNodeType.LITERAL)
-        false_branch.attributes['value'] = 0
-        
-        cond_node.attributes['condition'] = condition
-        cond_node.attributes['true_branch'] = true_branch
-        cond_node.attributes['false_branch'] = false_branch
-
-        # Infer type
-        inferred_type = self.analyzer.infer_type(cond_node)
-        self.assertEqual(inferred_type, 'void')
+        """Test type inference for conditionals."""
+        condition = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': True}
+        )
+        true_branch = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 1}
+        )
+        false_branch = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 0}
+        )
+        cond_node = IRNode(
+            node_type=IRNodeType.CONDITIONAL,
+            attributes={
+                'condition': condition,
+                'true_branch': true_branch,
+                'false_branch': false_branch,
+            },
+        )
+        self.ir.root.add_child(cond_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(cond_node)
+        self.assertEqual(inferred_type, 'int')
 
     def test_loop_type_inference(self):
         """Test type inference for loops."""
-        # Create nodes for loop
-        loop_node = IRNode(IRNodeType.LOOP)
-        condition = IRNode(IRNodeType.LITERAL)
-        condition.attributes['value'] = True
-        body = IRNode(IRNodeType.LITERAL)
-        body.attributes['value'] = 42
-        
-        loop_node.attributes['condition'] = condition
-        loop_node.attributes['body'] = body
-
-        # Infer type
-        inferred_type = self.analyzer.infer_type(loop_node)
+        condition = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': True}
+        )
+        body = IRNode(
+            node_type=IRNodeType.FUNCTION_CALL,
+            attributes={'function': IRNode(
+                node_type=IRNodeType.FUNCTION_DEF,
+                attributes={'name': 'do_something', 'return_type': 'void'}
+            )},
+        )
+        loop_node = IRNode(
+            node_type=IRNodeType.LOOP,
+            attributes={
+                'condition': condition,
+                'body': body,
+            },
+        )
+        self.ir.root.add_child(loop_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(loop_node)
         self.assertEqual(inferred_type, 'void')
 
     def test_function_def_type_inference(self):
         """Test type inference for function definitions."""
-        # Create function definition node
-        func_def = IRNode(IRNodeType.FUNCTION_DEF)
-        param = IRNode(IRNodeType.VARIABLE)
-        body = IRNode(IRNodeType.LITERAL)
-        body.attributes['value'] = 42
-        
-        func_def.attributes['parameters'] = [param]
-        func_def.attributes['body'] = body
-        func_def.attributes['return_type'] = 'int'
-
-        # Infer type
-        inferred_type = self.analyzer.infer_type(func_def)
-        self.assertEqual(inferred_type, 'int')
-
-    def test_program_node_type_inference(self):
-        """Test type inference for program nodes."""
-        program_node = IRNode(IRNodeType.PROGRAM)
-        self.assertIsNone(self.analyzer.infer_type(program_node))
+        func_def_node = IRNode(
+            node_type=IRNodeType.FUNCTION_DEF,
+            attributes={
+                'name': 'compute',
+                'return_type': 'float',
+                'parameters': [],
+                'body': IRNode(
+                    node_type=IRNodeType.RETURN,
+                    attributes={'value': IRNode(
+                        node_type=IRNodeType.LITERAL,
+                        attributes={'value': 3.14}
+                    )},
+                ),
+            },
+        )
+        self.ir.root.add_child(func_def_node)
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(func_def_node)
+        self.assertEqual(inferred_type, 'float')
 
     def test_complex_expression_type_inference(self):
         """Test type inference for complex expressions."""
-        # Create a complex expression: (1 + 2.0) * 3
-        mul_node = IRNode(IRNodeType.BINARY_OPERATION)
-        add_node = IRNode(IRNodeType.BINARY_OPERATION)
-        int_lit1 = IRNode(IRNodeType.LITERAL)
-        float_lit = IRNode(IRNodeType.LITERAL)
-        int_lit2 = IRNode(IRNodeType.LITERAL)
+        left_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 2}
+        )
+        right_node = IRNode(
+            node_type=IRNodeType.LITERAL, attributes={'value': 3.5}
+        )
+        bin_op_node = IRNode(
+            node_type=IRNodeType.BINARY_OPERATION,
+            attributes={
+                'operator': '*',
+                'left_operand': left_node,
+                'right_operand': right_node,
+            },
+        )
+        assign_node = IRNode(
+            node_type=IRNodeType.ASSIGNMENT,
+            attributes={
+                'left': IRNode(
+                    node_type=IRNodeType.VARIABLE, attributes={'name': 'result'}
+                ),
+                'right': bin_op_node,
+            },
+        )
+        self.ir.root.add_child(assign_node)
+        self.analyzer.analyze(self.ir)
+        var_node = assign_node.attributes['left']
+        inferred_type = self.analyzer.type_information.get(var_node)
+        self.assertEqual(inferred_type, 'float')
 
-        int_lit1.attributes['value'] = 1
-        float_lit.attributes['value'] = 2.0
-        int_lit2.attributes['value'] = 3
+    def test_program_node_type_inference(self):
+        """Test that the program node does not get a type."""
+        self.analyzer.analyze(self.ir)
+        inferred_type = self.analyzer.type_information.get(self.ir.root)
+        self.assertIsNone(inferred_type)
 
-        add_node.attributes['operator'] = '+'
-        add_node.attributes['left_operand'] = int_lit1
-        add_node.attributes['right_operand'] = float_lit
-
-        mul_node.attributes['operator'] = '*'
-        mul_node.attributes['left_operand'] = add_node
-        mul_node.attributes['right_operand'] = int_lit2
-
-        # First infer type of the addition
-        add_type = self.analyzer.infer_type(add_node)
-        self.assertEqual(add_type, 'float')  # int + float = float
-
-        # Then infer type of the multiplication
-        mul_type = self.analyzer.infer_type(mul_node)
-        self.assertEqual(mul_type, 'float')  # float * int = float
 
 if __name__ == '__main__':
     unittest.main()
