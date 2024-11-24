f421d95d264e2d9521a783754a906f884db72614
sungwookim <rkwk0112@gmail.com>
2024-11-24 14:45:41 +0900

    Register SwiftFrontend in FrontendRegistry

--- lapa/frontends/__init__.py
+++ lapa/frontends/__init__.py
@@ -1,13 +1,19 @@
 """
-Language frontend implementations.
+Initialize frontends and register them with the FrontendRegistry.
 """
 
-from .python import PythonFrontend
-from .javascript import JavaScriptFrontend
-from .cpp import CPPFrontend
+from lapa.frontend import FrontendRegistry
+from lapa.frontends.python import PythonFrontend
+from lapa.frontends.javascript import JavaScriptFrontend
+from lapa.frontends.cpp import CppFrontend
+from lapa.frontends.rust import RustFrontend
+from lapa.frontends.java import JavaFrontend
+from lapa.frontends.swift import SwiftFrontend
 
-__all__ = [
-    "PythonFrontend",
-    "JavaScriptFrontend",
-    "CPPFrontend"
-]
+# Register frontends
+FrontendRegistry.register("Python", PythonFrontend)
+FrontendRegistry.register("JavaScript", JavaScriptFrontend)
+FrontendRegistry.register("Cpp", CppFrontend)
+FrontendRegistry.register("Rust", RustFrontend)
+FrontendRegistry.register("Java", JavaFrontend)
+FrontendRegistry.register("Swift", SwiftFrontend)
